6f8f5f03dbda5ad834cfa5e7d0b5a5d107d03760
bhorselenberg <b.horselenberg@pernexus.org>
2018-01-29 22:26:49 +0100

    initial dev commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+/**
+ * import bootstrap
+ */
+require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
